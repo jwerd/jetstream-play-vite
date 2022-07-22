cd9a64a0e93947f1bd218dc21d20acf4a845ad26
Jake Vivian <jacob@sadsoft.com>
2022-07-21 20:10:03 -0700

    Init

---
+++ resources/views/app.blade.php
@@ -0,0 +1,19 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full bg-white">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+        <!-- Scripts -->
+        @routes
+        @vite('resources/js/app.js')
+        @inertiaHead
+    </head>
+    <body class="font-sans antialiased h-full overflow-hidden">
+        @inertia
+    </body>
+</html>
